df9920f8bf4700318ff0f0a5af3f73033425e90a
Rodolfo Souza <83579016+rodhfr@users.noreply.github.com>
2023-07-26 18:21:42 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/rodhfr/ytdlp-for-jellyfin.git
+
+# Install pip
+sudo apt install -y python3-pip
+
+# Install required packages from requirements.txt
+pip3 install -r requirements.txt
+
+# Create a "scripts" directory in the user's home folder
+mkdir -p ~/scripts
+
+# Copy ytdlp_functions.sh to the "scripts" directory
+cp ytdlp_functions.sh ~/scripts/
+
+# Append the contents of appendtobashrc.txt to .bashrc
+cat appendtobashrc.txt >> ~/.bashrc
+
+echo "Setup completed successfully!"
